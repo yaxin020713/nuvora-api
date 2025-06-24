321d803725563c30678c75746e0f3b34b4d95af4
yaxin020713 <yaxinzhu2002@gmail.com>
2025-06-24 11:18:38 +0800

    Update app.py

--- app.py
+++ app.py
@@ -41,4 +41,5 @@ def add_health_data():
     return jsonify({'message': 'Health data stored successfully'}), 201
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+
