5e841acbbff6bb99aedd1155e24d6bdc5bd171e1
yaxin020713 <yaxinzhu2002@gmail.com>
2025-06-24 11:20:26 +0800

    Update app.py

--- app.py
+++ app.py
@@ -40,6 +40,7 @@ def add_health_data():
     db.session.commit()
     return jsonify({'message': 'Health data stored successfully'}), 201
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
 
